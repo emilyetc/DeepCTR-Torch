2994b60b4814847bd17e0aeecfba3ee9de5021ce
emilyetc <41274138+emilyetc@users.noreply.github.com>
2024-08-02 11:10:45 -0700

    Update callbacks.py
    remove tensorflow dependency

--- deepctr_torch/callbacks.py
+++ deepctr_torch/callbacks.py
@@ -1,73 +1,73 @@
 import torch
-from tensorflow.python.keras.callbacks import EarlyStopping
-from tensorflow.python.keras.callbacks import ModelCheckpoint
-from tensorflow.python.keras.callbacks import History
+import os
 
-EarlyStopping = EarlyStopping
-History = History
-
-class ModelCheckpoint(ModelCheckpoint):
-    """Save the model after every epoch.
-
-    `filepath` can contain named formatting options,
-    which will be filled the value of `epoch` and
-    keys in `logs` (passed in `on_epoch_end`).
-
-    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
-    then the model checkpoints will be saved with the epoch number and
-    the validation loss in the filename.
-
+class ModelCheckpoint:
+    """Save the PyTorch model after every epoch.
+    
+    `filepath` can contain named formatting options, which will be filled with the value of `epoch` and keys in `logs` (passed in `on_epoch_end`).
+    
     Arguments:
         filepath: string, path to save the model file.
         monitor: quantity to monitor.
         verbose: verbosity mode, 0 or 1.
-        save_best_only: if `save_best_only=True`,
-            the latest best model according to
-            the quantity monitored will not be overwritten.
-        mode: one of {auto, min, max}.
-            If `save_best_only=True`, the decision
-            to overwrite the current save file is made
-            based on either the maximization or the
-            minimization of the monitored quantity. For `val_acc`,
-            this should be `max`, for `val_loss` this should
-            be `min`, etc. In `auto` mode, the direction is
-            automatically inferred from the name of the monitored quantity.
-        save_weights_only: if True, then only the model's weights will be
-            saved (`model.save_weights(filepath)`), else the full model
-            is saved (`model.save(filepath)`).
+        save_best_only: if `save_best_only=True`, the latest best model according to the quantity monitored will not be overwritten.
+        mode: one of {auto, min, max}. If `save_best_only=True`, the decision to overwrite the current save file is made based on either the maximization or the minimization of the monitored quantity. For `val_acc`, this should be `max`, for `val_loss` this should be `min`, etc. In `auto` mode, the direction is automatically inferred from the name of the monitored quantity.
+        save_weights_only: if True, then only the model's weights will be saved (`model.state_dict()`), else the full model is saved using `torch.save`.
         period: Interval (number of epochs) between checkpoints.
     """
-
+    
+    def __init__(self, filepath, monitor='val_loss', verbose=0, save_best_only=False, mode='auto', save_weights_only=False, period=1):
+        self.filepath = filepath
+        self.monitor = monitor
+        self.verbose = verbose
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.period = period
+        self.epochs_since_last_save = 0
+        self.best = float('inf') if mode == 'min' else -float('inf')
+        self.monitor_op = min if mode == 'min' else max
+        
+        if mode == 'auto':
+            if 'acc' in self.monitor:
+                self.monitor_op = max
+                self.best = -float('inf')
+            else:
+                self.monitor_op = min
+                self.best = float('inf')
+    
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
         self.epochs_since_last_save += 1
+        
         if self.epochs_since_last_save >= self.period:
             self.epochs_since_last_save = 0
             filepath = self.filepath.format(epoch=epoch + 1, **logs)
+            
             if self.save_best_only:
                 current = logs.get(self.monitor)
                 if current is None:
-                    print('Can save best model only with %s available, skipping.' % self.monitor)
+                    if self.verbose > 0:
+                        print('Can save best model only with {} available, skipping.'.format(self.monitor))
                 else:
-                    if self.monitor_op(current, self.best):
+                    if self.monitor_op(current, self.best) != self.best:
                         if self.verbose > 0:
-                            print('Epoch %05d: %s improved from %0.5f to %0.5f,'
-                                  ' saving model to %s' % (epoch + 1, self.monitor, self.best,
-                                                           current, filepath))
+                            print('Epoch {:05d}: {} improved from {:.5f} to {:.5f}, saving model to {}'.format(epoch + 1, self.monitor, self.best, current, filepath))
                         self.best = current
-                        if self.save_weights_only:
-                            torch.save(self.model.state_dict(), filepath)
-                        else:
-                            torch.save(self.model, filepath)
+                        self._save_model(epoch, filepath)
                     else:
                         if self.verbose > 0:
-                            print('Epoch %05d: %s did not improve from %0.5f' %
-                                  (epoch + 1, self.monitor, self.best))
+                            print('Epoch {:05d}: {} did not improve from {:.5f}'.format(epoch + 1, self.monitor, self.best))
             else:
                 if self.verbose > 0:
-                    print('Epoch %05d: saving model to %s' %
-                          (epoch + 1, filepath))
-                if self.save_weights_only:
-                    torch.save(self.model.state_dict(), filepath)
-                else:
-                    torch.save(self.model, filepath)
+                    print('Epoch {:05d}: saving model to {}'.format(epoch + 1, filepath))
+                self._save_model(epoch, filepath)
+    
+    def _save_model(self, epoch, filepath):
+        directory = os.path.dirname(filepath)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        if self.save_weights_only:
+            torch.save(self.model.state_dict(), filepath)
+        else:
+            torch.save(self.model, filepath)
